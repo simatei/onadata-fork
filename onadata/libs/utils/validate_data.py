d7e63679d49a6344b4b8801c77bb49f19b430bb0
Robert Kimutai <robertkimutai7@gmail.com>
2020-10-16 13:19:19 +0300

    add function to check gbif occurrence

--- onadata/libs/utils/validate_data.py
+++ onadata/libs/utils/validate_data.py
@@ -3,6 +3,21 @@ import xmltodict
 import json
 
 
+def check_gbif_data(name):
+    '''check whether a name exists in gbif database'''
+
+    url = "https://api.gbif.org/v1/occurrence/search"
+    querystring = {"scientificName": "{}".format(name)}
+    payload = ""
+    response = requests.request("GET", url, data=payload, params=querystring)
+
+    occurences = response.json()
+
+    occurence_list = set([name['species'] for name in occurences['results']])
+
+    return occurence_list
+
+
 def validate_data(xml):
     '''Function that validates data'''
     xml_str = json.dumps(xmltodict.parse(xml))
@@ -18,11 +33,29 @@ def validate_data(xml):
                                      for interaction in submission_json['repeat_group']]
 
                 if insect_name_other:
-                    return insect_name_other
+                    for name in set(insect_name_other):
+
+                        if name in check_gbif_data(name):
+                            valid.append('T')
+                        else:
+                            valid.append('F')
 
                 else:
                     return []
             except:
                 pass
+        else:
+            try:
+
+                name = interaction['capture_insect_details']['insect_scientific_name_other']
+                if name in check_gbif_data(name):
+                    valid.append('T')
+                else:
+                    valid.append('F')
+            except:
+                pass
+
+        return valid
+        
     except:
         pass
